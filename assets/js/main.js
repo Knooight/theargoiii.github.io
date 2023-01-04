5b3b88be5f41cab9900815a340222fb7f6dc57f2
Knoight <xafsanlegend5@gmail.com>
2023-01-04 19:28:50 +0600

    nav and screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,25 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     // localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// ==================ACTIVE LINK=======================
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
